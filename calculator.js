1aa8858efae3e76a74ca68541c0cc830a4a48c2c
KiyaG <hamilee.kiya@gmail.com>
2017-09-20 12:58:15 +0100

    the add function returns 0, the single number or the sum of the numbers for more than one arguments

--- calculator.js
+++ calculator.js
@@ -1,2 +1,9 @@
-module.exports.add = function (numbers) {
-};
+module.exports.add = function(n){
+  if(n.length < 2)
+    return n == "" ? 0 : Number(n);
+  else{ 
+      return n.split(/\n|,/).reduce((a,b)=>{
+        return Number(a) + Number(b);
+      })
+    }
+}
--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,15 @@ const calculator = require('./calculator');
 test('returns 0 for empty string', () => {
   expect(calculator.add("")).toBe(0);
 });
+test('returns the given number for single argument',()=>{
+  expect(calculator.add("1")).toBe(1);
+});
+test('returns their sum for two arguments',()=>{
+  expect(calculator.add("1,2")).toBe(3);
+});
+test('returns their sum for more than two arguments',()=>{
+  expect(calculator.add("2,3,5,7,8")).toBe(25);
+});
+test('handle new lines between numbers',()=>{
+  expect(calculator.add("1\n2,3")).toBe(6);
+})
